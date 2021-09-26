e95b97296eb2751d94cdae1154dd7851b8a656e3
Nadia Disla <tejadanadiam@gmail.com>
2021-09-25 21:47:59 -0400

    Add icons for iOS. Remove default comments.

--- book-finder-app/book-finder-app.Android/MainActivity.cs
+++ book-finder-app/book-finder-app.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace book_finder_app.Droid
 {
-    [Activity(Label = "book_finder_app", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "BookFinder", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
